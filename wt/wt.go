3e6a96ca0da0d06e3051fa6f9edfa61e1a2c63e8
Tors Dalid <torsdalid@gmail.com>
2018-12-23 20:09:27 +0800

    Elaborate helper routines

--- wt/wt.go
+++ wt/wt.go
@@ -219,9 +219,18 @@ func (r *ErrorResponse) Error() string {
 		r.Response.StatusCode, r.Message)
 }
 
-// Primitive helper routines that allocates a new primitive value
+// Bool is a helper routine that allocates a new bool value
 // to store v and returns a pointer to it.
-func Bool(v bool) *bool       { return &v }
+func Bool(v bool) *bool { return &v }
+
+// Int is a helper routine that allocates a new int value
+// to store v and returns a pointer to it.
+func Int(v int) *int { return &v }
+
+// Int64 is a helper routine that allocates a new int64 value
+// to store v and returns a pointer to it.
+func Int64(v int64) *int64 { return &v }
+
+// String is a helper routine that allocates a new string value
+// to store v and returns a pointer to it.
 func String(v string) *string { return &v }
-func Int(v int) *int          { return &v }
-func Int64(v int64) *int64    { return &v }
