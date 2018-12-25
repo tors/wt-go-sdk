904474c4cc057c1573d2382329b514dd53ccacea
Tors Dalid <torsdalid@gmail.com>
2018-12-25 12:38:15 +0800

    Test local files to see if valid file objects are returned

--- wt/file_object_test.go
+++ wt/file_object_test.go
@@ -58,3 +58,28 @@ func TestFromString_empty(t *testing.T) {
 		t.Errorf("FromString returned error %v", err)
 	}
 }
+
+func TestFromLocalFile(t *testing.T) {
+
+	tests := []struct {
+		file, name string
+		size       int64
+	}{
+		{"../example/files/Japan-01🇯🇵.jpg", "Japan-01.jpg", 13370099},
+		{"../example/files/Japan-02.jpg", "Japan-02.jpg", 275639},
+		{"../example/files/Japan-03.jpg", "Japan-03.jpg", 432557},
+	}
+
+	for _, c := range tests {
+		object, closer, _ := FromLocalFile(c.file)
+		defer closer()
+
+		if object.Name() != c.name {
+			t.Errorf("Name returned %v, want %v", object.Name(), c.name)
+		}
+
+		if object.Size() != c.size {
+			t.Errorf("Size returned %v, want %v", object.Size(), c.size)
+		}
+	}
+}
