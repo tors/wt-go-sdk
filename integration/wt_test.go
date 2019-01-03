8a2782ec1ac6c3bf13327b4cd1b0e991aebf8b50
Tors Dalid <torsdalid@gmail.com>
2019-01-03 17:38:06 +0800

    Start integration tests

---
+++ integration/wt_test.go
@@ -0,0 +1,50 @@
+// +build integration
+
+package integration
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/tors/wt-go-sdk/wt"
+)
+
+var (
+	client  *wt.Client
+	keyName = []byte("WETRANSFER_API_TOKEN")
+)
+
+func init() {
+	apiKey, err := readToken(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	logf(`Using key "%v"`, apiKey)
+	client, err = wt.NewAuthorizedClient(context.Background(), apiKey, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func readToken(file) (string, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return "", err
+	}
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return "", err
+	}
+	if !bytes.Equal(b[:len(keyName)], keyName) {
+		return "", fmt.Errorf("Key must be WETRANSFER_API_TOKEN")
+	}
+	return string(bytes.Trim(b[len(keyName)+1:], "\n")), nil
+}
+
+func logf(fmt string, args ...interface{}) {
+	log.Printf(fmt, args...)
+}
