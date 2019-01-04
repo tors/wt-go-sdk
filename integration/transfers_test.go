05453f017b312e7c6c6417ecb09951366df69648
Tors Dalid <torsdalid@gmail.com>
2019-01-04 16:25:44 +0800

    Integration tests for files

--- integration/transfers_test.go
+++ integration/transfers_test.go
@@ -27,3 +27,27 @@ func TestTransfers_Create_buffer(t *testing.T) {
 		t.Errorf("Transfer.GetURL is expected not be empty")
 	}
 }
+
+func TestTransfers_Create_files(t *testing.T) {
+	ctx := context.Background()
+
+	message := "Japan files"
+
+	files := []string{
+		"../example/files/Japan-01🇯🇵.jpg",
+		"../example/files/Japan-02.jpg",
+		"../example/files/Japan-03.jpg",
+	}
+
+	transfer, err := client.Transfers.Create(ctx, &message, files)
+	if err != nil {
+		t.Errorf("Transfers.Create returned an error %v", err)
+	}
+
+	url := transfer.GetURL()
+	logf("Got URL: %v", url)
+
+	if url == "" {
+		t.Errorf("Transfer.GetURL is expected not be empty")
+	}
+}
